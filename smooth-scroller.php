887b5eaf7e7e3b6f309444980b34742e7a09674f
alandloh <support@alephmedia.my>
2025-06-24 04:12:43 +0800

    Update smooth-scroller.php

--- smooth-scroller.php
+++ smooth-scroller.php
@@ -5,7 +5,7 @@
  Description:   Smooth scroller by Aleph Media.
  Author:        Aleph Media
  Author URI:    https://alephmedia.my
- Version:       0.2
+ Version:       0.4
  Update URI:    false
 
  GitHub Plugin URI:  https://github.com/alandloh/smooth-scroller
@@ -14,52 +14,54 @@
 
 
 wp_enqueue_scripts(
+add_action( 
+    'wp_enqueue_scripts', 
+    function() {
+            // The core GSAP library
+        wp_enqueue_script( 
+            'gsap-js', 
+            'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js', 
+            array(), 
+            '3.13.0', 
+            array(
+                'strategy' => 'defer',
+                'in_footer' => true
+            ) 
+        );
+        
+        // ScrollTrigger - with gsap.js passed as a dependency
+        wp_enqueue_script( 
+            'gsap-st', 
+            'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js', 
+            array('gsap-js'), 
+            '3.13.0', 
+            array(
+                'strategy' => 'defer',
+                'in_footer' => true
+            ) 
+        );
 
-    // The core GSAP library
-    wp_enqueue_script( 
-        'gsap-js', 
-        'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js', 
-        array(), 
-        '3.13.0', 
-        array(
-            'strategy' => 'defer',
-            'in_footer' => true
-        ) 
-    );
-    
-    // ScrollTrigger - with gsap.js passed as a dependency
-    wp_enqueue_script( 
-        'gsap-st', 
-        'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js', 
-        array('gsap-js'), 
-        '3.13.0', 
-        array(
-            'strategy' => 'defer',
-            'in_footer' => true
-        ) 
-    );
-
-    wp_enqueue_script(
-        'gsap-ss',
-        'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollSmoother.min.js',
-        array('gsap-js'),
-        '3.13.0', 
-        array(
-            'strategy' => 'defer',
-            'in_footer' => true
-        ) 
-    );
-    
-    // Your animation code file - with gsap.js passed as a dependency
-    wp_enqueue_script( 
-        'gsap-js2', 
-        plugin_dir_url(__FILE__) . 'js/app.js',
-        array('gsap-js'), 
-        null, 
-        array(
-            'strategy' => 'defer',
-            'in_footer' => true
-        ) 
-    );
-
+        wp_enqueue_script(
+            'gsap-ss',
+            'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollSmoother.min.js',
+            array('gsap-js'),
+            '3.13.0', 
+            array(
+                'strategy' => 'defer',
+                'in_footer' => true
+            ) 
+        );
+        
+        // Your animation code file - with gsap.js passed as a dependency
+        wp_enqueue_script( 
+            'gsap-js2', 
+            plugin_dir_url(__FILE__) . 'js/app.js',
+            array('gsap-js'), 
+            null, 
+            array(
+                'strategy' => 'defer',
+                'in_footer' => true
+            ) 
+        );
+    }
 );
